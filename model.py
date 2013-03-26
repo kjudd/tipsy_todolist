47d8d61a308b32e80549abaac1b197bd3cbd5d3c
Hackbright student <user@mini-005.(none)>
2013-03-25 17:59:29 -0700

    tipsy updated

--- model.py
+++ model.py
@@ -2,6 +2,7 @@
 model.py
 """
 import sqlite3
+from datetime import datetime
 
 def connect_db():
     return sqlite3.connect("tipsy.db")
@@ -9,8 +10,9 @@ def connect_db():
 def new_user(db, email, password, name):          
     c = db.cursor()                                     
     query = """INSERT INTO Users VALUES (NULL, ?, ?, ?)"""                                                           
-    c.execute(query, (email, password, name))           
+    result = c.execute(query, (email, password, name))           
     db.commit()
+    return result.lastrowid
 
 def authenticate(db, email, password):
     c = db.cursor()
@@ -18,26 +20,87 @@ def authenticate(db, email, password):
     c.execute(query, (email, password))
     result = c.fetchone()
     if result:
-        fields = ["id", "email", "password", "username"]
+        fields = ["id", "email", "password", "name"]
         return dict(zip(fields, result))
 
     return None
 
-def get_user(db, user_id):
-    """Gets a user dictionary out of the database given an id"""
-    pass
+def get_user(db, id):
+	c = db.cursor()
+	query = """SELECT * from Users WHERE id = ?"""
+	c.execute(query, (id,))
+	result = c.fetchone()
+	if result:
+		fields = ["id", "email", "password", "name"]
+		return dict(zip(fields, result))
+	else:
+		print "no user found"
 
 def new_task(db, title, user_id):
-    """Given a title and a user_id, create a new task belonging to that user. Return the id of the created task"""
-    pass
+	# Given a title and a user_id, create a new task belonging to that user. Return the id of the created task
+	c = db.cursor()     
+	date_time= datetime.now()
+	query = """INSERT INTO Tasks VALUES (NULL, ?, ?, NULL, ?)"""                                                           
+	result = c.execute(query, (title, date_time, user_id))           
+	db.commit()
+	return result.lastrowid
 
-def complete_task(db, task_id):
-    """Mark the task with the given task_id as being complete."""
-    pass
 
-def get_tasks(db, user_id=None):
-    """Get all the tasks matching the user_id, getting all the tasks in the system if the user_id is not provided. Returns the results as a list of dictionaries."""
-    pass
+def complete_task(db, id):
+	"""Mark the task with the given task_id as being complete."""
+	c = db.cursor()     
+	date_time = datetime.now()
+	query = """UPDATE Tasks SET completed_at = ? WHERE id = ?"""                                                           
+	result = c.execute(query, (id, date_time))           
+	db.commit()
+	print "Task completed at %s" % date_time
+	return result.lastrowid
+	
+def get_tasks(db, user_id):
 
-def get_task(db, task_id):
-    """Gets a single task, given its id. Returns a dictionary of the task data."""
+	"""Get all the tasks matching the user_id, getting all the tasks in the system if the user_id is not provided. Returns the results as a list of dictionaries."""
+	c = db.cursor()
+	if user_id:
+		query = """SELECT * from Tasks WHERE user_id = ?"""
+		c.execute(query, (user_id,))
+	else:
+		query = """SELECT * from Tasks"""
+		c.execute(query)
+	tasks = []
+	rows = c.fetchall()
+	for row in rows:
+		task = dict(zip(["id", "title", "created_at", "completed_at",
+			"user_id"], row))
+		tasks.append(task)
+	return tasks
+	# result = c.fetchone()
+	# if result:
+	# 	fields = ["id", "title", "created_at", "completed_at", "user_id"]
+	# 	return dict(zip(fields, result))
+	# else:
+	# 	c = db.cursor()
+	# 	query = """SELECT * from Tasks"""
+	# 	c.execute(query, )
+	# 	result = c.fetchall()
+	# 	l = []
+	# 	for row in result:
+	# 		new_dict = {}
+	# 		new_dict['id']=row[0]
+	# 		new_dict['title']=row[1]
+	# 		new_dict['created_at']=row[2]
+	# 		new_dict['completed_at']=row[3]
+	# 		new_dict['user_id']=row[4]
+	# 		l.append(new_dict)
+	# 	return l
+
+def get_task(db, id):
+	"""Gets a single task, given its id. Returns a dictionary of the task data."""
+	c = db.cursor()
+	query = """SELECT * from Tasks WHERE id = ?"""
+	c.execute(query, (id,))
+	result = c.fetchone()
+	if result:
+		fields = ["id", "title", "created_at", "completed_at", "user_id"]
+		return dict(zip(fields, result))
+	else:
+		print "task id not found"
--- tipsy.py
+++ tipsy.py
@@ -1,18 +1,43 @@
 """
 tipsy.py -- A flask-based todo list
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import model
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", user_name="chriszf")
+    return render_template("index.html", user_name="zivi")
 
 @app.route("/tasks")
 def list_tasks():
-    return render_template("list_tasks.html")
+	db = model.connect_db()
+	tasks_from_db = model.get_tasks(db, None)
+	return render_template("list_tasks.html", tasks = tasks_from_db)
+
+@app.route("/new_task")
+def new_tasks():
+	return render_template("new_task.html")
+
+@app.route("/save_task", methods = ["POST"])
+def save_task():
+	task_title = request.form['task_title']
+	db = model.connect_db()
+	task_id = model.new_task(db, task_title, 1)
+	return redirect("/tasks")
+
+@app.route("/complete_task", methods = ["POST"])
+def finished_task():
+	db = model.connect_db()
+	tasks_from_db = model.get_tasks(db, None)
+	print tasks_from_db
+	for task in tasks_from_db:
+		if request.form['complete']:
+			
+			model.complete_task(db, task)
+	return redirect("/tasks")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
